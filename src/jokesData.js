fa60d62c5beb278eff82c7d871d59b25fcfe5134
cabonilla <christian.bonilla01@gmail.com>
2020-12-23 13:28:20 -0800

    Initial commit.

---
+++ src/jokesData.js
@@ -0,0 +1,33 @@
+const jokesData = [
+  {
+      id: 1,
+      punchLine: "I'm exhausted with you, Laverne."
+  },
+  {
+      id: 2,
+      question: "Hello",
+      punchLine: "Laverne."
+  },
+  {
+      id: 3,
+      question: "Goodbye",
+      punchLine: "Laverne."
+  },
+  {
+      id: 4,
+      question: "Hello again",
+      punchLine: "Laverne."
+  },
+  {
+      id: 5,
+      question: "Bye again",
+      punchLine: "Laverne."
+  },
+  {
+      id: 6,
+      question: "Again",
+      punchLine: "Laverne?"
+  }
+]
+
+export default jokesData
